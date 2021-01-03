be27eaeb16e8dac49df3b0904acb858120a8d335
ea0273 <76868962+ea0273@users.noreply.github.com>
2021-01-03 15:25:18 +0900

    Update read_bot.py

--- read_bot.py
+++ read_bot.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-import asyncio
-import os
 import subprocess
 import ffmpeg
 from voice_generator import creat_WAV
@@ -20,55 +18,29 @@ async def on_ready():
 
 @client.command()
 async def join(ctx):
-    print('#join')
-    print('#voicechannelを取得')
+    #voicechannelを取得
     vc = ctx.author.voice.channel
-    print('#voicechannelに接続')
+    #voicechannelに接続
     await vc.connect()
 
 @client.command()
 async def bye(ctx):
-    print('#bye')
-    print('#切断')
+    #切断
     await ctx.voice_client.disconnect()
-
-@client.command()
-async def register(ctx, arg1, arg2):
-    with open('C:/open_jtalk/bin/dic.txt', mode='a') as f:
-        f.write('\n'+ arg1 + ',' + arg2)
-        print('dic.txtに書き込み：''\n'+ arg1 + ',' + arg2)
-    await ctx.send('`' + arg1+'` を `'+arg2+'` として登録しました')
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    server_id_test = "サーバーID"
-    text_id_test = "通知させたいテキストチャンネルID"
-
-
-    if member.guild.id == server_id_test:   # サーバーid
-        text_ch = client.get_channel(text_id_test)   # 通知させたいTEXTチャンネルid
-        if before.channel is None:
-            msg = f'【VC参加ログ】{member.name} が {after.channel.name} に参加しました。'
-            await text_ch.send(msg)
 
 @client.event
 async def on_message(message):
-    print('---on_message_start---')
-    msgclient = message.guild.voice_client
-    print(msgclient)
     if message.content.startswith('.'):
         pass
 
     else:
         if message.guild.voice_client:
-            print('#message.content:'+ message.content)
+            print(message.content)
             creat_WAV(message.content)
             source = discord.FFmpegPCMAudio("output.wav")
             message.guild.voice_client.play(source)
         else:
             pass
-    await client.process_commands(message)
-    print('---on_message_end---')
 
 
-client.run("トークン")
+client.run("NzQzMTExOTU0MTA0NTgyMzA1.XzP68A.Jc3vqKNjS1V0cmy0rQlTtgoJU6M")
